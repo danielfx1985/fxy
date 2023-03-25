a76e7b94e19ac406b99a1d82b5700b32f84cd739
danielfx1985 <wzr_ok@163.com>
2023-03-25 09:46:40 +0800

    0320

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -88,7 +88,7 @@ USE_MODELTRANSLATION = False
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["localhost", "127.0.0.1","47.92.105.68"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1","47.92.105.68","www.ynfxyxsbnfy.org.cn","ynfxyxsbnfy.org.cn"]
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
